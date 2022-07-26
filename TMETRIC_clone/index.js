3a2fdcd3720111020765ee6acfcda364eae5c8e4
nitin <nitinsharma754010@gmail.com>
2022-07-26 18:20:30 +0530

    clone

---
+++ TMETRIC_clone/index.js
@@ -0,0 +1,8 @@
+//login button click event
+let login=document.getElementById("Login").addEventListener("click", function(){
+    window.location.href="loginpage.html"
+})
+
+let signup=document.getElementById("Signup").addEventListener("click", function(){
+    window.location.href="signup.html"
+})
